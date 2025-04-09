321bfe398f9f742051414383f12408d8e122565d
ldyislucky <1278950466@qq.com>
2025-04-09 15:26:58 +0800

    将聊天记录持久化到了redis

--- src/agentapi/agent/langchat.py
+++ src/agentapi/agent/langchat.py
@@ -1,5 +1,12 @@
+
+from fastapi import APIRouter
 from src.agentapi.utils.redis_tool import get_conversation_chain
 
-redis_chain = get_conversation_chain("user_123")
-response1 = redis_chain.run("请问我的第一个问题是什么？")
-print(response1)
+router = APIRouter(prefix="/agent", tags=["agent"])
+
+@router.post("/chat")
+async def query_redis(user_id: str,question: str):
+    redis_chain = get_conversation_chain(user_id)
+    response1 = redis_chain.run(question)
+    print(response1)
+    return {"response": response1}
